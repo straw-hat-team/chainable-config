0dee4fb42eda469d117b9e72cad2fd856bcd42cc
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-11-25 05:53:13 -0500

    always return an string

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -4,12 +4,13 @@ export function shouldShortenFunction(value: any, verbose: boolean) {
   return typeof value === 'function' && !verbose && value.toString().length > 100;
 }
 
-export function toString(config: any, options?: { verbose: boolean }) {
+export function toString(config: any, options?: { verbose?: boolean }) {
   const verbose = options?.verbose ?? false;
-  return stringify(config, (value, _indent, next) => {
+  const output = stringify(config, (value, _indent, next) => {
     if (shouldShortenFunction(value, verbose)) {
       return `function () { /* omitted long function */ }`;
     }
     return next(value);
   });
+  return output ?? '';
 }
